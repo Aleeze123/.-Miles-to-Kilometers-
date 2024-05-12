e0e07f0f34c51279fb3952bf5082863006cc0ddd
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-05-13 03:55:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//Miles-to-Kilometer converter 
+// Create a variable "miles" and assign a numeric value
+var miles = 130;
+// Calculate distance in kilometer using conversion factor
+var kilometers = miles * 1.60934;
+// Print the message with the converted distance.
+console.log("The distance of ".concat(miles, " miles is equal to ").concat(kilometers.toFixed(4), " kilometers."));
---
+++ index.ts
@@ -0,0 +1,10 @@
+//Miles-to-Kilometer converter 
+
+// Create a variable "miles" and assign a numeric value
+let miles = 130;
+
+// Calculate distance in kilometer using conversion factor
+let kilometers = miles * 1.60934;
+
+// Print the message with the converted distance.
+console.log(`The distance of ${miles} miles is equal to ${kilometers.toFixed(4)} kilometers.`);
